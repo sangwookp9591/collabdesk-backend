97ab0c3f6f9b7b6ddb81add7a9bc1a2695287e7b
sangwookpark <sangwookp9591@gmail.com>
2025-08-24 23:36:02 +0900

    feat : prisma auth supabase add

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
+// app.module.ts (깔끔해진 버전)
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { SupabaseModule } from './supabase/supabase.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    SupabaseModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
