e5574fffb3cf01efebd38201305843b7baa35a03
sangwookpark <sangwookp9591@gmail.com>
2025-08-25 15:25:15 +0900

    feat : server root page 안내

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,13 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    return `
+    <title>CollabDesk Server</title>
+    <h2>CollabDesk Server</h2>
+    안녕하세요 Collab-Desk 서버입니다 
+    <br/>
+    <br/>
+    자세한 API 호출 안내는 <a href="/api">/api</a>로 접속`;
   }
 }
