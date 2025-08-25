7f81933cb0e75608c67ee15775f7ff2711981e64
sangwookpark <sangwookp9591@gmail.com>
2025-08-25 19:22:17 +0900

    feat :JwtService module add

--- src/jwt-token/jwt-token.module.ts
+++ src/jwt-token/jwt-token.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { JwtTokenService } from './jwt-token.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  providers: [JwtTokenService],
+  providers: [JwtTokenService, JwtService],
   exports: [JwtTokenService],
 })
 export class JwtTokenModule {}
