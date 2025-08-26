599837671300ede7645078e5d29b564b7c7a40bc
sangwookpark <sangwookp9591@gmail.com>
2025-08-26 22:13:25 +0900

    feat: ChannelModule 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { AuthModule } from './auth/auth.module';
 import { JwtTokenModule } from './jwt-token/jwt-token.module';
 import { WorkspaceModule } from './workspace/workspace.module';
 import { UserModule } from './user/user.module';
+import { ChannelModule } from './channel/channel.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { UserModule } from './user/user.module';
     JwtTokenModule,
     WorkspaceModule,
     UserModule,
+    ChannelModule,
   ],
   controllers: [AppController],
   providers: [AppService],
