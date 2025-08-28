d566a423f96203b8ef1bd46e7447c39e57ba1789
sangwookpark <sangwookp9591@gmail.com>
2025-08-28 18:05:38 +0900

    feat : message Controller와 getMessagesByChannel service추가

---
+++ src/message/message.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/jwt-token/guards/jwt-auth.guard';
+import { MessageService } from './message.service';
+
+@Controller('message')
+@UseGuards(JwtAuthGuard)
+export class MessageController {
+  constructor(private readonly messageService: MessageService) {}
+
+  @Get('channel:channelId')
+  async getMessagesByChannel(@Param('channelId') channelId: string) {
+    return await this.messageService.getMessagesByChannel(channelId);
+  }
+}
--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -1,7 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class MessageService {
   constructor(private prisma: PrismaService) {}
+
+  async getMessagesByChannel(channelId: string) {
+    const channel = await this.prisma.channel.findUnique({
+      where: { id: channelId },
+    });
+
+    if (!channel) {
+      throw new NotFoundException('채널을 찾을 수 없습니다.');
+    }
+
+    const userFields = {
+      id: true,
+      email: true,
+      name: true,
+      status: true,
+      profileImageUrl: true,
+    };
+
+    const messages = await this.prisma.message.findMany({
+      where: {
+        channelId: channelId,
+        parentId: null, //상위 메시지만
+      },
+      include: {
+        user: {
+          select: userFields,
+        },
+        replies: {
+          // 스레드 포함
+          include: { user: { select: userFields } },
+          orderBy: { createdAt: 'asc' },
+        },
+      },
+      orderBy: { createdAt: 'asc' }, // 최신순 혹은 오름차순
+    });
+    return messages;
+  }
 }
