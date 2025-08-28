4ea098ac92d3c6921ff653ba3f42f8db18c8e319
sangwookpark <sangwookp9591@gmail.com>
2025-08-28 20:19:16 +0900

    refactor : message module 수정

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -5,9 +5,19 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { JwtTokenModule } from 'src/jwt-token/jwt-token.module';
 import { UserModule } from 'src/user/user.module';
 import { UserService } from 'src/user/user.service';
+import { MessageController } from './message.controller';
+import { JwtAuthGuard } from 'src/jwt-token/guards/jwt-auth.guard';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [PrismaModule, UserModule, JwtTokenModule],
-  providers: [MessageGateway, MessageService, UserService],
+  controllers: [MessageController],
+  providers: [
+    MessageGateway,
+    MessageService,
+    UserService,
+    JwtAuthGuard,
+    JwtService,
+  ],
 })
 export class MessageModule {}
