8fd9f5c1a817aeba9a80e49fff81043d1d7c02a8
sangwookpark <sangwookp9591@gmail.com>
2025-08-30 21:01:49 +0900

    feat : redis module 추가

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MessageRedisService } from './message-redis.service';
+import { InviteRedisService } from './invite-redis.service';
 
 @Module({
-  providers: [MessageRedisService],
-  exports: [MessageRedisService],
+  providers: [MessageRedisService, InviteRedisService],
+  exports: [MessageRedisService, InviteRedisService],
 })
 export class RedisModule {}
