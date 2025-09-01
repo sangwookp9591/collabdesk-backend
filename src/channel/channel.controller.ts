27f4633152a7fab879d6185fa260422f5818750c
sangwookpark <sangwookp9591@gmail.com>
2025-09-01 15:14:22 +0900

    feat : invite member api

--- src/channel/channel.controller.ts
+++ src/channel/channel.controller.ts
@@ -8,12 +8,17 @@ import {
   Delete,
   UseGuards,
   Req,
+  UnauthorizedException,
+  BadRequestException,
+  Query,
+  ValidationPipe,
 } from '@nestjs/common';
 import { ChannelService } from './channel.service';
 import { CreateChannelDto } from './dto/create-channel.dto';
 import { UpdateChannelDto } from './dto/update-channel.dto';
 import { JwtAuthGuard } from 'src/jwt-token/guards/jwt-auth.guard';
 import type { Request } from 'express';
+import { InviteChannelDto } from './dto/invite-channel.dto';
 
 @Controller('channel')
 @UseGuards(JwtAuthGuard)
@@ -24,6 +29,44 @@ export class ChannelController {
   async reate(@Req() req: Request, @Body() createChannelDto: CreateChannelDto) {
     const userId = req.user!.sub;
     return await this.channelService.create(createChannelDto, userId);
+  }
+
+  @Get('invite')
+  async getInviteChannel(@Req() req: Request, @Query('code') code: string) {
+    const email = req.user?.email;
+    if (!email) {
+      throw new UnauthorizedException('이메일 정보가 없습니다.');
+    }
+
+    if (!(code && code.length === 6)) {
+      throw new BadRequestException('잘못된 요청입니다.');
+    }
+
+    return await this.channelService.getInviteChannel(email, code);
+  }
+
+  @Post('invite')
+  async inviteChannel(
+    @Req() req: Request,
+    @Body(ValidationPipe) dto: InviteChannelDto,
+  ) {
+    const userId = req.user?.sub;
+    if (!userId) {
+      throw new UnauthorizedException('이용자 정보가 없습니다.');
+    }
+
+    return await this.channelService.inviteChannel(userId, dto);
+  }
+
+  @Post('invite/join')
+  async joinChannelByCode(@Req() req: Request, @Body('code') code: string) {
+    const userId = req.user?.sub;
+    const email = req.user?.email;
+    if (!(userId && email)) {
+      throw new UnauthorizedException('이용자 정보가 없습니다.');
+    }
+
+    return await this.channelService.joinChannelByCode(userId, email, code);
   }
 
   @Get(':slug')
