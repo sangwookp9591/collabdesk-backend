c2fdd39d17ca01f75ff2c41a4d430dff7cadb3e6
sangwookpark <sangwookp9591@gmail.com>
2025-09-03 14:39:27 +0900

    feat : message controller api수정 및 recent message조회 api추가

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -4,26 +4,43 @@ import {
   Get,
   Param,
   Query,
+  Req,
   UseGuards,
 } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/jwt-token/guards/jwt-auth.guard';
 import { MessageService } from './message.service';
 import { GetMessagesQueryDto } from './dto/get-message-by-channel';
+import type { Request } from 'express';
 
-@Controller('message')
+@Controller()
 @UseGuards(JwtAuthGuard)
 export class MessageController {
   constructor(private readonly messageService: MessageService) {}
 
-  @Get('channel/:slug')
+  @Get('workspaces/:slug/channels/:channelSlug/messages')
   async getMessagesByChannel(
     @Param('slug') slug: string,
+    @Param('channelSlug') channelSlug: string,
     @Query() dto: GetMessagesQueryDto,
   ) {
-    return await this.messageService.getMessagesByChannel(slug, dto);
+    return await this.messageService.getMessagesByChannel(channelSlug, dto);
   }
 
-  @Delete(':id')
+  @Get('workspaces/:slug/messages/recent')
+  async getRecentMessages(
+    @Req() req: Request,
+    @Query('workspaceSlug') workspaceSlug: string,
+    @Query('take') take?: string,
+  ) {
+    const userId = req.user.sub;
+    return await this.messageService.getRecentMessages(
+      userId,
+      workspaceSlug,
+      Number(take ?? 10),
+    );
+  }
+
+  @Delete('messages/:id')
   async remove(@Param('id') id: string) {
     return await this.messageService.remove(id);
   }
