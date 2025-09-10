23b4dded83f79521973d0bd36e0e8cfda8c7dc1c
sangwookpark <sangwookp9591@gmail.com>
2025-09-10 19:47:06 +0900

    feat : message module에NotificationQueue추가

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -8,6 +8,7 @@ import { SupabaseModule } from 'src/supabase/supabase.module';
 import { MailModule } from 'src/mail/mail.module';
 import { SocketModule } from 'src/socket/socket.module';
 import { WorkspaceModule } from 'src/workspace/workspace.module';
+import { NotificationQueue } from 'src/worker/notification/notification.queue';
 @Module({
   imports: [
     PrismaModule,
@@ -19,7 +20,7 @@ import { WorkspaceModule } from 'src/workspace/workspace.module';
     WorkspaceModule,
   ],
   controllers: [MessageController],
-  providers: [MessageService],
+  providers: [MessageService, NotificationQueue],
   exports: [MessageService],
 })
 export class MessageModule {}
